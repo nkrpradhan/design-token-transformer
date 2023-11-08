b96e74182d55adb780e52becb58b2fa2857c4492
nkrpradhan <nkrpradhan@users.noreply.github.com>
2023-11-08 19:45:18 +0000

    token test

--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,12 +4,13 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 08 May 2023 07:37:37 GMT
+// Generated on Wed, 08 Nov 2023 19:45:18 GMT
 
 
 #import "StyleDictionarySize.h"
 
 
+float const TypographyBodyVariableFontFontWeight = 500;
 float const TypographyBodyH3FontWeight = 700;
 float const TypographyBodyH4StrikeThroughFontWeight = 500;
 float const TypographyBodyItalicFontWeight = 400;
